1727b8eb10f54c75b919cca5ecd218e596fabe62
Danilo Faraum <112991335+DaniloFaraum@users.noreply.github.com>
2023-05-17 11:08:10 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+let pedra = document.getElementById("pedra").value
+let papel = document.getElementById("papel")
+let tesoura = document.getElementById("tesoura")
+
+
+document.getElementById("choose").addEventListener("click", () =>{
+    console.log(pedra);
+})
